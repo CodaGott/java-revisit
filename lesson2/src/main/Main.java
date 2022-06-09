712d18344cdb109e4e5ce792b08f47fa9a04e69a
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2022-06-09 21:42:03 +0100

    lesson 2

---
+++ lesson2/src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+        primitives - numbers, chars, boolean
+        references - objects
+         */
+
+        byte b1;  // 8-bits
+        short s1; // 16-bits
+        int i1; // 32-bits
+        long l1; // 64-bits
+    }
+}
