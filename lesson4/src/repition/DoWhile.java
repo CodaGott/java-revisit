e5c3d68f3f38fe9841889d1904e04632eeb2d0a4
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2022-06-15 22:56:04 +0100

    operators

---
+++ lesson4/src/repition/DoWhile.java
@@ -0,0 +1,16 @@
+package repition;
+
+public class DoWhile {
+    public static void main(String[] args) {
+
+        //TODO:::>>> &&,   &,    ||,   |,   !
+        int i = 1;
+
+        do {
+            System.out.println("This must run!");
+            i++;
+        }
+        while (i < 0);
+        System.out.println("I is " + i);
+    }
+}
