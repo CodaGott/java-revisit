57288b547e3e8f136ab2267c106ec881deffd7ef
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2022-06-15 23:01:55 +0100

    logic operators

--- lesson4/src/repition/DoWhile.java
+++ lesson4/src/repition/DoWhile.java
@@ -3,7 +3,7 @@ package repition;
 public class DoWhile {
     public static void main(String[] args) {
 
-        //TODO:::>>> &&,   &,    ||,   |,   !
+        //TODO:::>>> Logical operators. (&&,   &,    ||,   |,   !)
         int i = 1;
 
         do {
@@ -12,5 +12,22 @@ public class DoWhile {
         }
         while (i < 0);
         System.out.println("I is " + i);
+
+        //TODO: not operator (!)
+        // ---changes boolean to the opposite value.---
+
+        boolean b = false;
+
+        boolean result = !b;
+
+        System.out.println("Result is: " +result);
+
+        int e = 9;
+        int f = 2;
+
+        boolean result2 = !(e > f);
+
+        System.out.println("Result 2 : "+ result2);
+
     }
 }
