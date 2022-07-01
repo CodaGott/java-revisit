1f0bd5e88ea54e9fef9a2792664178ef5ed92251
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2022-07-01 08:50:19 +0100

    update

---
+++ lesson6/src/decrementNincrement/Example1.java
@@ -0,0 +1,12 @@
+package decrementNincrement;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+        var i = 10;
+
+        System.out.println(i = i+ 1);
+
+        System.out.println(i);
+    }
+}
